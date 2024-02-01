13829c3a84fc837bb23dd6ae67f974c75a02b645
Gabriel Oliveira <gabriel.oliveira.dse@gmail.com>
2024-02-01 17:05:15 +0100

    Added chat list and pokeball icon

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,55 @@
 package com.example.myapplication
 
+// I added:
+
+
+// it was already at the project
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize //
-import androidx.compose.material3.MaterialTheme //
-import androidx.compose.material3.Surface //
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.tutorial.SampleData
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                Conversation(SampleData.conversationSample)
             }
 
         }
@@ -31,22 +60,87 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Text(text = msg.author)
-    Text(text = msg.body)
-}
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(id = R.drawable.pika),
+            contentDescription = "Contact profile picture",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primaryContainer, CircleShape)
+                //.fillMaxSize(1)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+        var isExpanded by remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Gabriel")
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                style = MaterialTheme.typography.titleSmall
+            )
+            //Text(text = msg.author)
+            Spacer(modifier = Modifier.height(8.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium, tonalElevation = 1.dp
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+
+
+                )
+            }
+        }
     }
 }
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+    //Text(
+            //text = "Hello $name!",
+            //modifier = modifier
+    //)
+//}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn{
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+
+}
+
+
+@Preview( name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    MyApplicationTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Gabriel", "Les MEAnimaux!")
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    MyApplicationTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
